67ce34849dc8d6023cb369a07194220aa2bfa8d9
Kai Aragaki <adamaragaki@gmail.com>
2023-10-17 09:10:34 -0400

    add hazard stuff

--- scratch.R
+++ scratch.R
@@ -1,55 +1,34 @@
-library(ggplot2)
-library(bladdr)
+shape <- 5
+scale <- 1
+t0 <- 1.999
 
-# Figure: A small collection of
-# time-to-cat-singing:
-set.seed(1)
-n <- 5
-df <- data.frame(
-  cat = 1:n,
-  sing_time = rgamma(n, 2)
+results <- data.frame(
+  t = c(1.001, 1.05, 1.10, 1.15, 1.20, 1.25, 2, 10)
 )
-ggplot(df, aes(x = sing_time, y = cat)) +
-  geom_segment(aes(x = 0, y = cat, xend = sing_time, yend = cat)) +
-  geom_point() +
-  theme_tufte(10) +
-  labs(x = "Cat age (human years)",
-       y = "Cat") +
-  theme(axis.ticks.y = element_blank(),
-        axis.text.y = element_blank())
 
-set.seed(2)
-n <- 100
-df <- data.frame(
-  cat = 1:n,
-  sing_time = rgamma(n, 2)
-)
-ggplot(df, aes(x = sing_time, y = cat)) +
-  geom_segment(aes(x = 0, y = cat, xend = sing_time, yend = cat)) +
-  geom_point() +
-  theme_tufte(10) +
-  labs(x = "Cat age (human years)",
-       y = "Cat") +
-  theme(axis.ticks.y = element_blank(),
-        axis.text.y = element_blank())
+# Compute exact probabilities of death at times t
+# Conditional on survival to time t0
+results$probability <- results |>
+  with((pweibull(t, shape) - pweibull(t0, shape)) / (1 - pweibull(t0, shape)))
 
+# hazard at time t
+h_t <- function(t) {
+  scale^(-shape) * shape * t^(shape - 1)
+}
 
+plot(1:10, h_t(1:10))
 
+hazard  <- h_t(2)
 
+results$approximation <- hazard * (results$t - t0)
 
+results
 
-
-df <- data.frame(gs = rgamma(100000, 2))
-
-
-ggplot(df, aes(gs)) +
-  geom_density() +
-  theme_tufte(10) +
-  labs(x = "Cat age (human years)",
-       y = "Probability of singing") +
-  theme(axis.ticks.y = element_blank(),
-        axis.text.y = element_blank())
-
-
-ggplot(data.frame(x = c(0, 20)), aes(x)) +
-  stat_function(fun = dgamma, args = c(2))
+# I understand what h(t)dt stands for
+# I thought it would be a more accurate
+# descriptor of conditional survival for some
+# length of time the smaller dt was.
+# It's actually a more accurate descriptor of
+# conditional surival for *dt* the smaller *dt* is
+#
+# Now to figure out what (F(t) - F(t0))/S(t0) is
